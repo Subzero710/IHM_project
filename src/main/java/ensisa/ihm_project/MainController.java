2613f6c9c72be7a2e2cfc36be3b24f7872350b42
Jean <novellojean75@gmail.com>
2025-12-16 11:22:04 +0100

    menu+ quitter

--- src/main/java/ensisa/ihm_project/MainController.java
+++ src/main/java/ensisa/ihm_project/MainController.java
@@ -1,9 +1,14 @@
 package ensisa.ihm_project;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainController {
+    @FXML
+    private void quitMenuAction() {
+        Platform.exit();
+    }
     @FXML
     private Label welcomeText;
 
